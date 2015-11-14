f8daea6e306d49d30b82153b45b992571d4e840a
SauloAmorim <saulo.amorim99@hotmail.com>
2015-11-14 17:14:17 -0200

    comitado em casa!

---
+++ Terceira Certificação/Feitos em Casa/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variaveis
+            Usuarios u = new Usuarios();
+            bool query = false;
+            string user = "";
+            string senha = "";
+
+            // Início do Código
+            Console.WriteLine("Bem vindo ao Programa");
+            Console.Write("Insira o nome de usuário:");
+            user = Console.ReadLine();
+            Console.Write("Insira a senha:");
+            senha = Console.ReadLine();
+            
+            // Saída de Acerto
+            if (query == true)
+            {
+                Console.WriteLine("Bem Vindo {0}", user);
+                Console.WriteLine("Sua ultima entrada foi em {0}", u.ultimoLogIn);
+            }
+
+            // Saída de Erro
+            if (query == false)
+            {
+                Console.WriteLine("Senha ou Usuário não encontrado");
+            }
+
+        }
+    }
+}
