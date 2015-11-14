cc454ed179278df8cebb59df41d23d12e587f873
SauloAmorim <saulo.amorim99@hotmail.com>
2015-11-14 17:54:42 -0200

    comitado de casa

--- Terceira Certificação/Feitos em Casa/ConsoleApplication1/ConsoleApplication1/Program.cs
+++ Terceira Certificação/Feitos em Casa/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -13,28 +13,59 @@ namespace ConsoleApplication1
             // Variaveis
             Usuarios u = new Usuarios();
             bool query = false;
+            int opcoes = 0;
             string user = "";
             string senha = "";
 
             // Início do Código
             Console.WriteLine("Bem vindo ao Programa");
-            Console.Write("Insira o nome de usuário:");
-            user = Console.ReadLine();
-            Console.Write("Insira a senha:");
-            senha = Console.ReadLine();
+            while (true)
+            {
+                Console.Write("Insira o nome de usuário:");
+                user = Console.ReadLine();
+                Console.Write("Insira a senha:");
+                senha = Console.ReadLine();
+                query = true;
             
-            // Saída de Acerto
-            if (query == true)
-            {
-                Console.WriteLine("Bem Vindo {0}", user);
-                Console.WriteLine("Sua ultima entrada foi em {0}", u.ultimoLogIn);
-            }
+                // Saída de Login feito com sucesso
+                if (query == true)
+                {
+                    Console.WriteLine("Login feito com sucesso!");
+                    Console.WriteLine("Bem-Vindo(a) {0}", user);
+                    Console.WriteLine("Sua ultima entrada foi em {0}", u.ultimoLogIn);
+                    // Loop da tela de opções
+                    while (opcoes != 4)
+                    {
+                        Console.WriteLine("O Que deseja fazer? \r\n[1]- Acessar as Credenciais \r\n[2]- Alterar as Credenciais \r\n[3]- Modificar \r\n[4]- Sair ");
+                        opcoes = int.Parse(Console.ReadLine());
+                        switch (opcoes)
+                        {
+                            case 1:
+                                Console.WriteLine("SERVIÇO INDISPONÍVEL");
+                                break;
+                            case 2:
+                                Console.WriteLine("SERVIÇO INDISPONÍVEL");
+                                break;
+                            case 3:
+                                Console.WriteLine("SERVIÇO INDISPONÍVEL");
+                                break;
+                            case 4:
+                                break;
+                            default:
+                                Console.WriteLine("Digite uma das opções");
+                                break;
+                        }
+                    }
+                
+                }
 
-            // Saída de Erro
-            if (query == false)
-            {
-                Console.WriteLine("Senha ou Usuário não encontrado");
+                // Saída de Erro
+                if (query == false)
+                {
+                    Console.WriteLine("Senha ou Usuário não encontrado");
+                }                
             }
+            
 
         }
     }
